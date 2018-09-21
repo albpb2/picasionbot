0186054050f72d25fcc023e03e11a826c85b639c
a.paz <a.paz@payvision.com>
2018-09-21 12:25:06 +0200

    Exit controller created.

---
+++ PicasionBot/PicasionBot/Controllers/ExitController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PicasionBot.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ExitController : ControllerBase
+    {
+        private readonly IApplicationLifetime appLifetime;
+
+        public ExitController(IApplicationLifetime appLifetime)
+        {
+            this.appLifetime = appLifetime;
+        }
+
+        [HttpPost]
+        public IActionResult Exit()
+        {
+            appLifetime.StopApplication();
+            return Ok();
+        }
+    }
+}
