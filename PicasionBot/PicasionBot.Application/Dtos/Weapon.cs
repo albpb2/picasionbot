3356b9991277d2e07499d962ccf4fac4c006bf12
a.paz <a.paz@payvision.com>
2018-09-21 12:05:56 +0200

    Weapon created.

---
+++ PicasionBot/PicasionBot.Application/Dtos/Weapon.cs
@@ -0,0 +1,16 @@
+﻿namespace PicasionBot.Application.Dtos
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    public class Weapon
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [BindProperty(Name = "ap_cost")]
+        public int CostInActionPoints { get; set; }
+        [BindProperty(Name = "ap_damage")]
+        public int DamagePerActionPoint { get; set; }
+        public Var Durability { get; set; }
+    }
+}
