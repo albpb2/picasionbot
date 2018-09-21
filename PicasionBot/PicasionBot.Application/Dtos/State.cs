3cba7984f336abfd76971ac746f3041f84a9f503
a.paz <a.paz@payvision.com>
2018-09-21 12:12:07 +0200

    State created.

---
+++ PicasionBot/PicasionBot.Application/Dtos/State.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PicasionBot.Application.Dtos
+{
+    public class State
+    {
+        public Var Turns { get; set; }
+        public IReadOnlyCollection<Warrior> Allies { get; set; }
+        public IReadOnlyCollection<Warrior> Enemies { get; set; }
+        public IReadOnlyCollection<Weapon> Weapons { get; set; }
+        public IReadOnlyCollection<Item> Items { get; set; }
+    }
+}
