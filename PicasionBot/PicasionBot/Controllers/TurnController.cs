555de96181a462e93180ffe7b24cf756bde86c41
a.paz <a.paz@payvision.com>
2018-09-21 13:02:35 +0200

    Turn controller created.

---
+++ PicasionBot/PicasionBot/Controllers/TurnController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace PicasionBot.Controllers
+{
+    using Application.Dtos;
+
+    [Route("[controller]")]
+    [ApiController]
+    public class TurnController : ControllerBase
+    {
+        [HttpPost]
+        public BaseAction Run(State state)
+        {
+            return new BaseAction();
+        }
+    }
+}
