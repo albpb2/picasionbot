780bf801511b50455b50155e9bf1da70660d3608
a.paz <a.paz@payvision.com>
2018-09-21 12:10:00 +0200

    Warrior created

---
+++ PicasionBot/PicasionBot.Application/Dtos/Warrior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PicasionBot.Application.Dtos
+{
+    public class Warrior
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public Var Health { get; set; }
+        public Var ActionPoints { get; set; }
+        public IReadOnlyCollection<Weapon> Weapons { get; set; }
+        public IReadOnlyCollection<Item> Items { get; set; }
+    }
+}
