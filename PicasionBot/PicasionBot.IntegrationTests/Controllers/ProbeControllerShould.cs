07e3b541ddba26fd0aae45ffa5981439573096fc
a.paz <a.paz@payvision.com>
2018-09-21 11:34:49 +0200

    Header removed from class

--- PicasionBot/PicasionBot.IntegrationTests/Controllers/ProbeControllerShould.cs
+++ PicasionBot/PicasionBot.IntegrationTests/Controllers/ProbeControllerShould.cs
@@ -1,16 +1,10 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="ProbeControllerShould.cs" company="Payvision">
-//     Payvision Copyright © 2018
-// </copyright>
-// -----------------------------------------------------------------------
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
 
 namespace PicasionBot.IntegrationTests.Controllers
 {
-    using System.Net.Http;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc.Testing;
-    using Xunit;
-
     public class ProbeControllerShould : IClassFixture<WebApplicationFactory<Startup>>
     {
         private const string ControllerBaseUri = "/probe";
